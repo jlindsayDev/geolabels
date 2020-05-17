47e965f1592458275af33882f32aedc4e5cbdc93
Josh Lindsay <git@j000sh.com>
2020-05-17 02:39:19 -0700

    move header/section to bottom-left

--- map/models.py
+++ map/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+  oauth_id = models.CharField(max_length=100)
+
+class Point(models.Model):
+  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+  name = models.CharField(max_length=100)
+  address = models.CharField(max_length=150)
+  longitude = models.DecimalField(decimal_places=7, max_digits=10)
+  latitude = models.DecimalField(decimal_places=7, max_digits=10)
+
+  POINT_TYPES = [
+    ('LI', 'Liked'),
+    ('SA', 'Saved'),
+    ('LV', 'Loved'),
+    ('FR', 'Friend')
+  ]
+  models.CharField(blank=True, choices=POINT_TYPES)
